3d93ed2aa66ec9fddbd03795dc54cf95504cfc8a
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2024-06-10 23:05:31 +0200

    update to setup script

--- net/setup.ps1
+++ net/setup.ps1
@@ -1,8 +1,17 @@
 # Start from root workspace folder
 
+param (
+    [switch]$delete = $false
+)
+
+# Configuration 
+
+if($delete) {
+    kind delete cluster
+}
+
 # Create cluster k8s with kind
 $kind_clusters = (kind get clusters | Out-String)
-Write-Host $kind_clusters
 if($kind_clusters -eq ''){
     kind create cluster --config .\net\kind-config.yaml
 }
@@ -17,6 +26,10 @@ if($LASTEXITCODE -ne 0){
     exit -1;
 }
 
+# Cleanup old docker images that have been replaced
+# TODO scope the pruning to just the images that have been replaced
+docker image prune --force
+
 # Load created images in docker
 kind load docker-image test-worker-express:1.0.0
 if($LASTEXITCODE -ne 0){
@@ -25,10 +38,13 @@ if($LASTEXITCODE -ne 0){
 }
 
 # Apply external configurations and namespaces
-# Potrebbero fallire in caso di mancato internet, possibile scaricarle e tenere un copia locale ( fare script )
+# Could fail if no internet access, TODO download file offline 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
+
+# Sleep to avoid timeout ( ingress-nginx has a timeout of 10 seconds from last request/download )
+Start-Sleep -Seconds 15
 
 # Apply internal configurations 
 kubectl apply -f .\net\k8s\
